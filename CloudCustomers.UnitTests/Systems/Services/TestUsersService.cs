fd5683c693f1958bdc7f8d90a98c6345ac112e08
johannpa <johann_paque@hotmail.fr>
2023-10-09 19:40:45 +0200

    Create a new folder in test for service and to test it with a new class TestUsersService

---
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -0,0 +1,27 @@
+﻿using CloudCustomers.API.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudCustomers.UnitTests.Systems.Services
+{
+    public class TestUsersService
+    {
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_InvokestHttpGetRequest()
+        {
+            // Arrange
+            var sut = new UsersService();
+
+            // Act
+            await sut.GetAllUsers();
+
+            // Assert
+            // Verify HTTP request is made !
+
+
+        }
+    }
+}
