19f0a103e1aa05927b6b18bec45e751f5f2f48c9
johannpa <johann_paque@hotmail.fr>
2023-10-13 17:40:49 +0200

    Add new test to get all users

--- CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
+++ CloudCustomers.UnitTests/Systems/Services/TestUsersService.cs
@@ -2,6 +2,7 @@
 using CloudCustomers.API.Services;
 using CloudCustomers.UnitTests.Fixtures;
 using CloudCustomers.UnitTests.Helpers;
+using FluentAssertions;
 using Moq;
 using Moq.Protected;
 using System;
@@ -37,5 +38,24 @@ namespace CloudCustomers.UnitTests.Systems.Services
                     ItExpr.IsAny<CancellationToken>()
                 );
         }
+
+        [Fact]
+        public async Task GetAllUsers_WhenCalled_ReturnsListOfUsers()
+        {
+            // Arrange
+            var expectedResponse = UsersFixture.GetTestUsers();
+            var handlerMock = MockHttpMessageHandler<User>.SetupBasicGetRessourceList(expectedResponse);
+            var httpClient = new HttpClient(handlerMock.Object);
+            var sut = new UsersService(httpClient);
+
+            // Act
+            var result = await sut.GetAllUsers();
+
+            // Assert
+            result.Should().BeOfType<List<User>>();
+        }
+
+
     }
 }
+
